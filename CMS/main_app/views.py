9cbe127041039db17cfeaee9cd36a2d533fb2900
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-05-25 15:23:12 -0500

    header viewandmodels

--- CMS/main_app/views.py
+++ CMS/main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Header
 
 # Create your views here.
+
+
+def header_input(request):
+    form = HeaderForm(request.POST)
+    if form.is_valid():
+        newheader = form.save(commit = False)
+        newheader.save()
+    return HttpResponseRedirect('/display')
+
+def index(request):
+    header = Header.objects.all()
+    return render(request, 'index.html', {'header': header})

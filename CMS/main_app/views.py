5a572d42ce13a5a44e3cfa9d9db0b8e03a0a4735
vpdacoder@gmail.com <vaipatel@utexas.edu>
2018-05-28 12:08:01 -0500

    Home page rendering the content correctly

--- CMS/main_app/views.py
+++ CMS/main_app/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 from .models import Header
 
-# Create your views here.
-
-
-# def header_input(request):
-#     form = HeaderForm(request.POST)
-#     if form.is_valid():
-#         newheader = form.save(commit = False)
-#         newheader.save()
-#     return HttpResponseRedirect('/display')
-
 def index(request):
+    return render(request, 'index.html')
+
+def home(request):
     header = Header.objects.all()
-    return render(request, 'index.html', { 'header': header })
+    return render(request, 'home.html',{'header':header})

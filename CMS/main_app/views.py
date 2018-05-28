db757222adab9eacd0a42518b6501cd64dca693f
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-05-28 16:53:16 -0500

    readme sauce

--- CMS/main_app/views.py
+++ CMS/main_app/views.py
@@ -4,14 +4,11 @@ from .models import Header
 # Create your views here.
 
 
-def header_input(request):
-    form = HeaderForm(request.POST)
-    if form.is_valid():
-        newheader = form.save(commit = False)
-        newheader.save()
-    return HttpResponseRedirect('/display')
-
-
 def index(request):
     header = Header.objects.all()
     return render(request, 'index.html', {'header': header})
+
+
+def home(request):
+    header = Header.objects.all()
+    return render(request, 'home.html',{'header':header})
